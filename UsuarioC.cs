5c86ef1497d11f479114f9d5af9a2569cc8acfbc
diogoalmeida219 <104276198+diogoalmeida219@users.noreply.github.com>
2022-11-18 13:00:20 -0300

    Add files via upload

---
+++ UsuarioC.cs
@@ -0,0 +1,57 @@
+﻿using CadCartas.Model;
+using CadCartas.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadCartas
+{
+    public class UsuarioC
+    {
+        UsuarioDao usuario_dao;
+        BancoM banco;
+
+        public UsuarioC (string banco)
+        {
+            banco = new BancoM ();
+            usuario_dao = new UsuarioDao(banco);
+        }
+        public void login (UsuarioM usuario)
+        {
+            
+        }
+
+        public void definir_banco(BancoM banco)
+        {
+            string aux;
+            aux = banco.banco;
+            usuario_dao = new UsuarioDao(aux);
+        }
+
+        public void inserir_usuario (UsuarioM usuario)
+        {
+            string aux;
+            aux=banco.banco;
+            usuario_dao = new UsuarioDao(aux);
+            usuario_dao.create_usuario(usuario);
+        }
+
+        public void alterar_usuario (UsuarioM usuario)
+        {
+            //crud
+        }
+
+        public void ler_usuario (UsuarioM usuario)
+        {
+            //crud
+        }
+
+        public void excluir_usuario (UsuarioM usuario)
+        {
+            //crud
+        }
+
+    }
+}
